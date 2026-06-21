7fd75342aae5c1520f1c21515819d60ca82bee6e
Adam R Contardo <lazerous42@gmail.com>
2026-06-21 03:10:21 -0400

    Added Mods: - Almore Unified - Better F3 - Cloth Config - CoFH Core - Controlling - EMI Ores - EnderIO - Immersive Engineering - KubeJS Thermal - Thermal Cultivation - Thermal Dynamics - Thermal Expansion - Thermal Foundation - Thermal Innovation - Thermal Integration - Thermal Locomotion - Vanilla Backport - VB Compat - Worldstripper
    Config Changes
    - Added immersiveengineering-server base config to defaultconfigs
    
    Scripting Modifcation:
    - Added orecleanup.js to remove duplicate ores from worldgen (Caverns & Chasms and IE ores)

---
+++ minecraft/kubejs/startup_scripts/orecleanup.js
@@ -0,0 +1,21 @@
+WorldgenEvents.remove((event) => {
+    event.removeOres(ores => {
+        // This targets the physical blocks being placed
+        ores.blocks = [
+            'caverns_and_chasms:deepslate_silver_ore',
+            'caverns_and_chasms:silver_ore',
+            'cgs:lead_ore',
+            'iceandfire:deepslate_silver_ore',
+            'iceandfire:silver_ore',
+            'occultism:ore_silver',
+            'occultism:ore_silver_deepslate'
+        ];
+    });
+    // This targets feature generation
+    event.removeFeatureById('underground_ores', [
+        'immersiveengineering:lead',
+        'immersiveengineering:silver',
+        'immersiveengineering:nickel',
+        'immersiveengineering:uranium'
+    ]);
+});
